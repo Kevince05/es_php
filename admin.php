e47926bb60d38a735d6771397ecb6f77be1b356f
unknown <in5b02@ILIDOM2018.LOCAL>
2023-11-25 11:27:32 +0100

    finalized admin console

--- admin.php
+++ admin.php
@@ -2,26 +2,27 @@
 
 session_start();
 
-$db = new mysqli("localhost", "root", "","cesco");
+$db = new mysqli("localhost", "root", "", "cesco");
 
 if ($db->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if(array_key_exists("REQUEST_TYPE", $_SERVER)){
-    if($_SERVER["REQUEST_TYPE"] === "POST"){
 
-        $usr = $_POST["usr"];
-        $scrt_lvl = $_POST["scrt_lvl"];
-    
-        if($_POST["submit_type"] == "Remove"){
-            if(!$db->query("DELETE FROM users WHERE username = '$usr'")){
-                echo "Something went wrong :(";
-                die("Something went wrong :(");
-            }
-        }else{
-            //TODO: add id to db, use id to delete and edit
-            $db->query("UPDATE table_name SET WHERE condition;");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $id = $_POST["id"];
+    $usr = $_POST["usr"];
+    $scrt_lvl = $_POST["scrt_lvl"];
+    if ($_POST["submit_type"] == "Remove") {
+        if (!$db->query("DELETE FROM users WHERE id = '$id'")) {
+            echo '<script>alert("Something went wrong :(")</script>';
+            die("Something went wrong :(");
+        }
+    } else {
+        if (!$db->query("UPDATE users SET username='$usr', security_lvl='$scrt_lvl' WHERE id = '$id'")) {
+            echo '<script>alert("Something went wrong :(")</script>';
+            die("Something went wrong :(");
         }
     }
 }
@@ -29,39 +30,43 @@ if(array_key_exists("REQUEST_TYPE", $_SERVER)){
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Admin</title>
-    </head>
-    <body>
+
+<head>
+    <title>Admin</title>
+</head>
+
+<body>
+    <?php
+    $usr = $_SESSION["usr"];
+    $result = $db->query("SELECT  security_lvl FROM users WHERE username = '$usr'");
+    if ($result->fetch_assoc()["security_lvl"] < 1) {
+        header("Location:index.php");
+        die("Not an admin");
+    } else {
+        echo "<h1>Admin Console</h1>";
+    }
+    ?>
+    <table>
+        <tr>
+            <th>Username</th>
+            <th>security_lvl</th>
+        </tr>
         <?php
-            $usr = $_SESSION["usr"];
-            $result = $db->query("SELECT  security_lvl FROM users WHERE username = '$usr'");
-            if($result->fetch_assoc()["security_lvl"] < 1){
-                header("Location:index.php");
-                die("Not an admin");
-            }else{
-                echo "<h1>Admin Console</h1>";
-            }            
-        ?>
-        <table>
-            <tr>
-                <th>Username</th>
-                <th>security_lvl</th>
-            </tr>
-            <?php
-                $result = $db->query("SELECT * FROM users");
-                foreach($result as $row){
-                    echo "<form action='admin.php' method='POST'>
+        $result = $db->query("SELECT * FROM users");
+        foreach ($result as $row) {
+            echo "<form action='admin.php' method='POST'>
                             <tr>
-                                <td><input type='text' value=".$row["username"]." name='usr'></td>
-                                <td><input type='number' value=".$row["security_lvl"]." name='scrt_lvl' min='0' max='1' step='1'></td>
+                                <input type='hidden' value=" . $row["id"] . " name='id'>
+                                <td><input type='text' value=" . $row["username"] . " name='usr'></td>
+                                <td><input type='number' value=" . $row["security_lvl"] . " name='scrt_lvl' min='0' max='1' step='1'></td>
                                 <td><input type='submit' name='submit_type' value='Edit'></td>
                                 <td><input type='submit' name='submit_type' value='Remove'></td>
                             </tr>
                           </form>";
-                }
-            ?>
-            
-        </table>
-    </body>
+        }
+        ?>
+
+    </table>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -1,53 +1,55 @@
-
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
-        <form action="login.php" method="POST">
-            Username: <input type="text" name="usr"><br>
-            Password: <input type="password" name="pwd"><br>
-            Admin:  <input type="checkbox" name="scrt_lvl"><br>
-            <input type="submit" name="submit_type" value="Login">
-            <input type="submit" name="submit_type" value="Register">
-        </form>   
-        <?php
-            session_start();
 
-            $db = new mysqli("localhost", "root", "","cesco");
-            
-            if ($db->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+<head>
+    <title>Login</title>
+</head>
 
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $usr = $_POST["usr"];
-                $pwd = md5($_POST["pwd"]);
-                
-                if($_POST["submit_type"] === "Login"){
-                    $result = $db->query("SELECT username, md5_password FROM users WHERE username='$usr' AND md5_password='$pwd'"); 
-                    if($result->num_rows == 1){
-                        $_SESSION["usr"] = $usr;
-                        $_SESSION["md5_pwd"] = $pwd;
-                        header("Location:index.php");
-                    }else{
-                        echo "Wrong user or password";
-                    }
-                }else{
-                    $scrt_lvl = $_POST["scrt_lvl"]?1:0;
-                    $result = $db->query("INSERT INTO users (username, md5_password, security_lvl) VALUE ('$usr', '$pwd', '$scrt_lvl')");
-                    if($result === TRUE){
-                        $_SESSION["usr"] = $usr;
-                        $_SESSION["md5_pwd"] = $pwd;
-                        $_SESSION["scrt_lvl"] = $scrt_lvl;
-                        header("Location:index.php");
-                    }else{
-                        echo "Error:" . $db -> error;
-                    }                     
-                }         
+<body>
+    <form action="login.php" method="POST">
+        Username: <input type="text" name="usr"><br>
+        Password: <input type="password" name="pwd"><br>
+        Admin: <input type="checkbox" name="scrt_lvl"><br>
+        <input type="submit" name="submit_type" value="Login">
+        <input type="submit" name="submit_type" value="Register">
+    </form>
+    <?php
+    session_start();
+
+    $db = new mysqli("localhost", "root", "", "cesco");
+
+    if ($db->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $usr = $_POST["usr"];
+        $pwd = md5($_POST["pwd"]);
+
+        if ($_POST["submit_type"] === "Login") {
+            $result = $db->query("SELECT username, md5_password FROM users WHERE username='$usr' AND md5_password='$pwd'");
+            if ($result->num_rows == 1) {
+                $_SESSION["usr"] = $usr;
+                $_SESSION["md5_pwd"] = $pwd;
+                header("Location:index.php");
+            } else {
+                echo "Wrong user or password";
             }
-        ?>
-    </body>
+        } else {
+            $scrt_lvl = $_POST["scrt_lvl"] ? 1 : 0;
+            $id = $db->query("SELECT max(id) FROM users")->fetch_row()[0] + 1;
+            $result = $db->query("INSERT INTO users (id, username, md5_password, security_lvl) VALUE ('$id', '$usr', '$pwd', '$scrt_lvl')");
+            if ($result === TRUE) {
+                $_SESSION["usr"] = $usr;
+                $_SESSION["md5_pwd"] = $pwd;
+                header("Location:index.php");
+            } else {
+                echo "Error:" . $db->error;
+            }
+        }
+    }
+    ?>
+</body>
+
 </html>
